927daf5e2a0097132fbae195b72fb92c2e4df29c
Phan Tiến Dũng <phantiendung.work@gmail.com>
2026-06-01 15:40:53 +0700

    mes and mestype

---
+++ src/common/Message.java
@@ -0,0 +1,51 @@
+package common;
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private MessageType type;
+    private String sender;
+    private String receiver;
+    private String content;
+
+    public Message(MessageType type, String sender, String receiver, String content) {
+        this.type = type;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    
+
+    public String getReceiver() {
+        return receiver;
+    }
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
---
+++ src/common/MessageType.java
@@ -0,0 +1,13 @@
+package common;
+
+public enum MessageType {
+    REGISTER,
+    LOGIN,
+    LOGOUT,
+    PRIVATE_CHAT,
+    GROUP_CHAT,
+    ONLINE_USERS,
+    FILE,
+    SERVER_MESSAGE,
+    ERROR
+}
